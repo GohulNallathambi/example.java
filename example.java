2cebe280a40afe77acc24b8693a19371680a1c81
GohulNallathambi <103426884+GohulNallathambi@users.noreply.github.com>
2022-04-12 12:38:16 +0530

    Update example.java

--- example.java
+++ example.java
@@ -4,5 +4,7 @@ public static void main(String arg[])
 	System.out.println("Hello Gooookulllleeee...");
 	System.out.println("Hello Gooookulllleeee...");
 	System.out.println("Hello Goooo");
+	System.out.println("Hello Goooo");
+	System.out.println("Hello nandhu");
 }
 }
