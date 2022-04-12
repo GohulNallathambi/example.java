346c93b2c448a860e61c910cd7924d4954d44465
GohulNallathambi <103426884+GohulNallathambi@users.noreply.github.com>
2022-04-12 16:26:01 +0530

    Update example.java

--- example.java
+++ example.java
@@ -6,5 +6,6 @@ public static void main(String arg[])
 	System.out.println("Hello Goooo");
 	System.out.println("Hello Goooo");
 	System.out.println("Hello nandhu");
+	System.out.println("Hello eso");
 }
 }
